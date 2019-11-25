36fa2be9fe640f1cf731422b8e30e9a50ede3513
Mrk992 <markis992@gmail.com>
2019-11-25 20:44:15 +0100

    First Commit

---
+++ src/main/java/club/chiringo/quiz/balon/oro/balonOro/BalonOroApplication.java
@@ -0,0 +1,13 @@
+package club.chiringo.quiz.balon.oro.balonOro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalonOroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalonOroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/chiringo/quiz/balon/oro/balonOro/BalonOroApplicationTests.java
@@ -0,0 +1,13 @@
+package club.chiringo.quiz.balon.oro.balonOro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalonOroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
